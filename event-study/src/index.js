b93135ab11482637af0718a6ea601a832bb1fb1e
jzw <706861648@qq.com>
2018-12-14 10:01:18 +0800

    finish event-study

--- event-study/src/index.js
+++ event-study/src/index.js
@@ -27,3 +27,18 @@ ReactDOM.render(
   <Toggle />,
   document.getElementById('root')
 );
+
+
+class LoggingButton extends React.Component {
+  handleClick = () => {
+    console.log('this is:', this);
+  }
+
+  render() {
+    return (
+      <button onClick = {(e) => this.handleClick(e)} >
+      Click me
+      </button>
+    );
+  }
+}
