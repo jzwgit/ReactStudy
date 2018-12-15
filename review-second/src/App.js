1ac2208b067db133fb76be51aae917dc8cbfd8b3
jzw <706861648@qq.com>
2018-12-15 18:08:33 +0800

    15号最后一次提交

--- review-second/src/App.js
+++ review-second/src/App.js
@@ -202,8 +202,86 @@ class Popper extends Component {
 // End : chapter 06 (事件处理)
 // -------------------------------------------------------------------
 // Start : chapter 07 (条件渲染)
+class UserGreeting extends Component {
+  render() {
+    return (
+      <h1>Welcome back ! </h1>
+    );
+  }
+}
+class GuestGreeting extends Component {
+  render() {
+    return (
+      <h1>Please sign up. </h1>
+    );
+  }
+}
+class LoginButton extends Component {
+  render (){
+    return (
+      <button onClick = {this.props.onClick} >
+	Login
+      </button>
+    );
+  }
+}
+class LogoutButton extends Component {
+  render () {
+    return (
+      <button onClick = {this.props.onClick}>
+	Logout
+      </button>
+    );
+  }
+}
+class Greeting extends Component {
+  constructor (props) {
+    super(props);
+  }
+  render() {
+    const isLoggedIn = this.props.isLoggedIn;
+    if (isLoggedIn){
+      return (
+	<UserGreeting />
+      );
+    }
+    return (
+      <GuestGreeting />
+    );
+  }
+}
+class LoginControl extends Component {
+  constructor (props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isLoggedIn: false};
+  }
+  handleLoginClick() {
+    this.setState({isLoggedIn: true});
+  }
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false});
+  }
 
-    
+  render () {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button = null;
+    if (isLoggedIn) {
+      button = <LogoutButton onClick = {this.handleLogoutClick} />;
+    } else {
+      button = <loginButton onClick = {this.handleLoginClick} />;
+    }
+
+    return (
+      <div>
+	<Greeting isLoggedIn = {isLoggedIn} />
+	{button}
+      </div>
+    );
+  }
+}
+
 
 
 
@@ -224,6 +302,8 @@ class App extends Component {
 	<ActionLink />
 	<Toggle />
 	<Popper />
+	<Greeting isLoggedIn = {true} />
+	<LoginControl />
       </div>
     );
   }
